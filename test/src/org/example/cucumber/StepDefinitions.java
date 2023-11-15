f26adc0b7f2eb355512d651402d2f7fa10fc902a
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2023-11-15 15:17:38 +0100

    test(Fridge): refactor variable names
    in step definitions to make them more readable.

--- test/src/org/example/cucumber/StepDefinitions.java
+++ test/src/org/example/cucumber/StepDefinitions.java
@@ -14,19 +14,19 @@ public class StepDefinitions {
     private int remainingBottles;
 
     @Given("I have {int} water bottles")
-    public void i_have_water_bottles(int int1) {
-        fridge = new Fridge(int1);
+    public void i_have_water_bottles(int bottles) {
+        fridge = new Fridge(bottles);
     }
     @Given("I have drunk {int} water bottles")
-    public void i_have_drunk_water_bottles(int int1) {
-        fridge.Take(int1);
+    public void i_have_drunk_water_bottles(int bottles) {
+        fridge.Take(bottles);
     }
     @When("I go to my fridge")
     public void i_go_to_my_fridge() {
         remainingBottles = fridge.getStock();
     }
     @Then("I should have {int} water bottles")
-    public void i_should_have_water_bottles(int int1) {
-        Assert.assertEquals(int1 , remainingBottles);
+    public void i_should_have_water_bottles(int bottles) {
+        Assert.assertEquals(bottles , remainingBottles);
     }
 }
